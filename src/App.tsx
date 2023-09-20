4229de3b4653c68eb2638af21bfecf3c8c19e3bf
yuma14 <yuma140902@gmail.com>
2023-09-20 22:41:15 +0900

    use theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import ImageSelector from './components/ImageSelector';
 import ImagePreview from './components/ImagePreview';
-import { Button, Layout, Space } from 'antd';
+import { Button, ConfigProvider, Layout, Space, Switch, theme } from 'antd';
 import { Content, Header } from 'antd/es/layout/layout';
 import cv from '@techstark/opencv-js';
 import useCvMatFromFile from './hooks/useCvMatFromFile';
 import { useWindowSize } from '@react-hook/window-size';
+import { useState } from 'react';
 
 function App() {
   const [cvMat, setCvMat, setImageFile] = useCvMatFromFile();
   const [windowWidth, windowHeight] = useWindowSize();
-  // design token: headerHeight
-  // https://ant.design/docs/react/customize-theme#customize-design-token
   const headerHeight = 64;
+  const [isDarkMode, setIsDarkMode] = useState(false); // TODO: ブラウザの設定
 
   const handleGrayscale = () => {
     if (cvMat) {
@@ -23,24 +23,45 @@ function App() {
   };
 
   return (
-    <Layout style={{ height: '100%' }}>
-      <Header>
-        <Space>
-          <Button onClick={handleGrayscale}>グレースケール</Button>
-        </Space>
-      </Header>
-      <Content>
-        {!cvMat ? (
-          <ImageSelector handleImageFile={(file) => setImageFile(file)} />
-        ) : (
-          <ImagePreview
-            image={cvMat}
-            width={windowWidth}
-            height={windowHeight - headerHeight}
-          />
-        )}
-      </Content>
-    </Layout>
+    <ConfigProvider
+      theme={{
+        algorithm: isDarkMode ? theme.darkAlgorithm : theme.defaultAlgorithm,
+        components: {
+          Layout: {
+            headerHeight: headerHeight,
+          },
+        },
+      }}
+    >
+      <Layout style={{ height: '100%' }}>
+        <Header style={{ width: '100%', display: 'flex' }}>
+          <Space>
+            <Button onClick={handleGrayscale}>グレースケール</Button>
+          </Space>
+          <Space style={{ float: 'right', marginLeft: 'auto' }}>
+            <Switch
+              checkedChildren="Dark"
+              unCheckedChildren="Light"
+              checked={isDarkMode}
+              onChange={(checked) => setIsDarkMode(checked)}
+            />
+          </Space>
+        </Header>
+        <Content>
+          {!cvMat ? (
+            <div style={{ padding: '2rem', height: '100%' }}>
+              <ImageSelector handleImageFile={(file) => setImageFile(file)} />
+            </div>
+          ) : (
+            <ImagePreview
+              image={cvMat}
+              width={windowWidth}
+              height={windowHeight - headerHeight}
+            />
+          )}
+        </Content>
+      </Layout>
+    </ConfigProvider>
   );
 }
 
