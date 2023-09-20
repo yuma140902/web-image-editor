bf351a1f5885788819983ff9de6e5d6b5a61914e
yuma14 <yuma140902@gmail.com>
2023-09-20 10:53:15 +0900

    ファイル追加画面

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,7 @@
+import { FileImageOutlined, InboxOutlined } from '@ant-design/icons'
 import { DatePicker } from 'antd'
+import { RcFile } from 'antd/es/upload'
+import Dragger from 'antd/es/upload/Dragger'
 import { useEffect, useRef, useState } from 'react'
 import { Image, Layer, Rect, Stage, Text } from 'react-konva'
 
@@ -40,11 +43,28 @@ function URLImage({ src, x, y }: URLImageProps) {
   return <Image x={x} y={y} image={image} />
 }
 
+
 function App() {
+  const [imageFile, setImageFile] = useState<RcFile | null>(null);
   const [count, setCount] = useState(0)
 
   return (
     <div>
+      <Dragger name="file" multiple={false} maxCount={1} showUploadList={false}
+        beforeUpload={(file) => {
+          console.log(file);
+          setImageFile(file);
+          return false;
+        }}
+      >
+        <p className='ant-upload-drag-icon'>
+          <FileImageOutlined />
+        </p>
+        <p className='ant-upload-text'>
+          クリックまたはここへドラッグして画像を選択
+        </p>
+        <p className='ant-upload-hint'>aiueo</p>
+      </Dragger>
       <DatePicker />
       <button onClick={() => setCount((count) => count + 1)}>
         count is {count}
@@ -53,10 +73,14 @@ function App() {
         <Layer>
           <Text text="text on canvas" />
           <Rect x={20} y={50} width={100} height={100} fill="green" shadowBlur={10} />
-          <URLImage src="https://konvajs.org/assets/lion.png" x={0} y={0} />
+          {
+            imageFile !== null ?
+              <URLImage src={window.URL.createObjectURL(imageFile.slice())} x={0} y={0} />
+              : undefined
+          }
         </Layer>
       </Stage>
-    </div>
+    </div >
   )
 }
 
