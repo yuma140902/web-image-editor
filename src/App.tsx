48c9c680a2d5886846833b52bfcdda7ba5e55fd9
yuma14 <yuma140902@gmail.com>
2023-09-20 11:07:59 +0900

    add ImageSelector component

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,17 @@
-import { FileImageOutlined } from '@ant-design/icons'
 import { RcFile } from 'antd/es/upload';
-import Dragger from 'antd/es/upload/Dragger';
 import { useState } from 'react'
 import { Layer, Stage } from 'react-konva'
 import URLImage from './components/URLImage';
+import ImageSelector from './components/ImageSelector';
 
 function App() {
   const [imageFile, setImageFile] = useState<RcFile | null>(null);
 
   if (imageFile === null) {
     return (
-      <Dragger name="file" multiple={false} maxCount={1} showUploadList={false}
-        beforeUpload={(file) => {
-          console.log(file);
-          setImageFile(file);
-          return false;
-        }}
-      >
-        <p className='ant-upload-drag-icon'>
-          <FileImageOutlined />
-        </p>
-        <p className='ant-upload-text'>
-          クリックまたはここへドラッグして画像を選択
-        </p>
-        <p className='ant-upload-hint'>aiueo</p>
-      </Dragger>
+      <ImageSelector handleImageFile={file => setImageFile(file)} />
     )
   }
-
   else {
     return (
       <div>
---
+++ src/components/ImageSelector.tsx
@@ -0,0 +1,26 @@
+import { FileImageOutlined } from "@ant-design/icons";
+import { RcFile } from "antd/es/upload";
+import Dragger from "antd/es/upload/Dragger";
+
+export type ImageSelectorProps = {
+  handleImageFile: (imageFile: RcFile) => void
+}
+
+export default function ImageSelector({ handleImageFile }: ImageSelectorProps) {
+  return (
+    <Dragger name="file" multiple={false} maxCount={1} showUploadList={false}
+      beforeUpload={(file) => {
+        handleImageFile(file);
+        return false;
+      }}
+    >
+      <p className='ant-upload-drag-icon'>
+        <FileImageOutlined />
+      </p>
+      <p className='ant-upload-text'>
+        クリックまたはここへドラッグして画像を選択
+      </p>
+      <p className='ant-upload-hint'>aiueo</p>
+    </Dragger>
+  )
+}
