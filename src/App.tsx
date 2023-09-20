a98abe0e4f16fc126155f794b9c39298e20fddf6
yuma14 <yuma140902@gmail.com>
2023-09-20 18:03:54 +0900

    add useCvMatFromFile

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,19 @@
-import { RcFile } from 'antd/es/upload';
-import { useEffect, useRef, useState } from 'react';
 import ImageSelector from './components/ImageSelector';
 import ImagePreview from './components/ImagePreview';
 import { Button, Layout, Space } from 'antd';
 import { Content, Header } from 'antd/es/layout/layout';
 import cv from '@techstark/opencv-js';
+import useCvMatFromFile from './hooks/useCvMatFromFile';
 
 function App() {
-  const [imageFile, setImageFile] = useState<RcFile | null>(null);
-
-  const imageElementRef = useRef<HTMLImageElement | null>(null);
-  const [imageElement, setImageElement] = useState<HTMLImageElement | null>(
-    null,
-  );
-  const [imageCvMat, setImageCvMat] = useState<cv.Mat | undefined>(undefined);
-
-  const loadImage = () => {
-    const imageBlobUrl = imageFile
-      ? window.URL.createObjectURL(imageFile.slice())
-      : undefined;
-
-    if (imageBlobUrl) {
-      const img = new window.Image();
-      img.src = imageBlobUrl;
-      img.crossOrigin = 'Anonymous';
-      imageElementRef.current = img;
-      imageElementRef.current.addEventListener('load', handleLoad);
-    }
-  };
-
-  const handleLoad = () => {
-    if (imageElementRef.current) {
-      setImageElement(imageElementRef.current);
-      setImageCvMat(cv.imread(imageElementRef.current));
-    }
-  };
-
-  useEffect(() => {
-    loadImage();
-    return () => {
-      if (imageElementRef.current) {
-        imageElementRef.current.removeEventListener('load', handleLoad);
-      }
-    };
-  }, []);
-
-  useEffect(() => {
-    loadImage();
-  }, [imageFile]);
+  const [imageCvMat, setImageCvMat, setImageFile] = useCvMatFromFile();
 
   const handleGrayscale = () => {
-    if (imageElementRef.current) {
-      const imgIn = cv.imread(imageElementRef.current);
+    if (imageCvMat) {
       const imgGray = new cv.Mat();
-      cv.cvtColor(imgIn, imgGray, cv.COLOR_BGR2GRAY);
+      cv.cvtColor(imageCvMat, imgGray, cv.COLOR_BGR2GRAY);
       imageCvMat?.delete();
       setImageCvMat(imgGray);
-      imgIn.delete();
     }
   };
 
@@ -68,7 +25,7 @@ function App() {
         </Space>
       </Header>
       <Content>
-        {imageFile === null ? (
+        {!imageCvMat ? (
           <ImageSelector handleImageFile={(file) => setImageFile(file)} />
         ) : (
           <ImagePreview image={imageCvMat} />
---
+++ src/hooks/useCvMatFromFile.ts
@@ -0,0 +1,52 @@
+import React, { useEffect, useRef, useState } from 'react';
+import cv from '@techstark/opencv-js';
+
+/*
+ * Fileをcv.Matに読み込むフック
+ */
+export default function useCvMatFromFile(
+  file?: File,
+): [
+  cv.Mat | undefined,
+  React.Dispatch<React.SetStateAction<cv.Mat | undefined>>,
+  React.Dispatch<React.SetStateAction<File | undefined>>,
+] {
+  const [imageFile, setImageFile] = useState<File | undefined>(file);
+  const dummyImageElementRef = useRef<HTMLImageElement | null>(null);
+  const [imageCvMat, setImageCvMat] = useState<cv.Mat | undefined>(undefined);
+
+  const loadImage = () => {
+    const blobUrl = imageFile
+      ? window.URL.createObjectURL(imageFile.slice())
+      : undefined;
+
+    if (blobUrl) {
+      const img = new window.Image();
+      img.src = blobUrl;
+      img.crossOrigin = 'Anonymous';
+      dummyImageElementRef.current = img;
+      dummyImageElementRef.current.addEventListener('load', handleLoad);
+    }
+  };
+
+  const handleLoad = () => {
+    if (dummyImageElementRef.current) {
+      setImageCvMat(cv.imread(dummyImageElementRef.current));
+    }
+  };
+
+  useEffect(() => {
+    loadImage();
+    return () => {
+      if (dummyImageElementRef.current) {
+        dummyImageElementRef.current.removeEventListener('load', handleLoad);
+      }
+    };
+  }, []);
+
+  useEffect(() => {
+    loadImage();
+  }, [imageFile]);
+
+  return [imageCvMat, setImageCvMat, setImageFile];
+}
