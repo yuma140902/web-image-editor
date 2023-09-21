e903b36f46447367658e26bf29e212092b278045
yuma14 <yuma140902@gmail.com>
2023-09-21 22:21:07 +0900

    update headers

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/web-image-editor/',
+  server: {
+    headers: {
+      'Strict-Transport-Security': 'max-age=86400; includeSubDomains', // Adds HSTS options to your website, with a expiry time of 1 day
+      'X-Content-Type-Options': 'nosniff', // Protects from improper scripts runnings
+      'X-Frame-Options': 'DENY', // Stops your site being used as an iframe
+      'X-XSS-Protection': '1; mode=block', // Gives XSS protection to legacy browsers
+    },
+  },
 });
