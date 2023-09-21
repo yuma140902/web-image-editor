b351d6515cb6cd2dc18e5869cc0181f802c82bd5
yuma14 <yuma140902@gmail.com>
2023-09-21 12:09:45 +0900

    ImagePreviewで画像を動かせるようにした

--- src/components/ImagePreview.tsx
+++ src/components/ImagePreview.tsx
@@ -4,7 +4,8 @@ import useImageBitmapFromCvMat from '../hooks/useImageBitmapFromCvMat';
 import { Project } from '../core/Project';
 import { FloatButton } from 'antd';
 import { MinusOutlined, PlusOutlined } from '@ant-design/icons';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
+import Konva from 'konva';
 
 export type ImagePreviewProps = {
   project: Project;
@@ -24,6 +25,10 @@ export default function ImagePreview({
 }: ImagePreviewProps) {
   const imageBitmap = useImageBitmapFromCvMat(project.mat);
   const [scale, setScale] = useState(1.0);
+  const [isDragging, setIsDragging] = useState(false);
+  const lastMousePos = useRef({ x: 0.0, y: 0.0 }); // TODO: useStateを試す
+  const [xOffset, setXOffset] = useState(0.0);
+  const [yOffset, setYOffset] = useState(0.0);
 
   const handleZoomIn = () => {
     setScale(scale + 0.1);
@@ -37,6 +42,32 @@ export default function ImagePreview({
     setScale(1.0);
   };
 
+  const handleMouseDown = (e: Konva.KonvaEventObject<MouseEvent>) => {
+    setIsDragging(true);
+    const posX = e.evt.pageX;
+    const posY = e.evt.pageY;
+    lastMousePos.current.x = posX;
+    lastMousePos.current.y = posY;
+  };
+
+  //TODO: 警告をなくす
+  const handleMouseUp = (_e: Konva.KonvaEventObject<MouseEvent>) => {
+    setIsDragging(false);
+  };
+
+  const handleMouseMove = (e: Konva.KonvaEventObject<MouseEvent>) => {
+    if (isDragging) {
+      const posX = e.evt.pageX;
+      const posY = e.evt.pageY;
+      const dx = posX - lastMousePos.current.x;
+      const dy = posY - lastMousePos.current.y;
+      setXOffset(xOffset + dx);
+      setYOffset(yOffset + dy);
+      lastMousePos.current.x = posX;
+      lastMousePos.current.y = posY;
+    }
+  };
+
   if (imageBitmap) {
     return (
       <>
@@ -44,14 +75,17 @@ export default function ImagePreview({
           width={width}
           height={height}
           className={`checker ${isDarkMode ? 'dark' : 'light'}`}
+          onMouseDown={handleMouseDown}
+          onMouseUp={handleMouseUp}
+          onMouseMove={handleMouseMove}
         >
           <Layer>
             <Image
               image={imageBitmap}
               scaleX={scale}
               scaleY={scale}
-              x={width / 2 - (imageBitmap.width * scale) / 2}
-              y={height / 2 - (imageBitmap.height * scale) / 2}
+              x={width / 2 - (imageBitmap.width * scale) / 2 + xOffset}
+              y={height / 2 - (imageBitmap.height * scale) / 2 + yOffset}
             />
           </Layer>
         </Stage>
